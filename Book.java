2070e1cbd99a1219889eb98513fca6b096524cc0
MOHAMMED ALHAMRASHDI <Malhamrashdi@gmail.com>
2026-08-11 14:56:37 +0400

    Book

--- Book.java
+++ Book.java
@@ -1,18 +1,23 @@
 package java_11;
 
-
 public class Book implements LibraryItem {
+
     private String title;
     private String author;
     private int pages;
+    private String shelfCode;
+
     public Book(String title, String author, int pages) {
         setTitle(title);
         setAuthor(author);
         setPages(pages);
+        setShelfCode(shelfCode);
     }
+
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         if (title == null || title.trim().isEmpty()) {
             IO.println("Name cannot be empty.");
@@ -20,15 +25,19 @@ public class Book implements LibraryItem {
             this.title = title;
         }
     }
+
     public String getAuthor() {
         return author;
     }
+
     public void setAuthor(String author) {
         this.author = author;
     }
+
     public int getPages() {
         return pages;
     }
+
     public void setPages(int pages) {
         if (pages > 0) {
             this.pages = pages;
@@ -37,21 +46,31 @@ public class Book implements LibraryItem {
             IO.println("Pages must be above 0");
         }
     }
+
+    public String getShelfCode() {
+        return shelfCode;
+    }
+
+    public void setShelfCode(String shelfCode) {
+        if (shelfCode == null || shelfCode.trim().isEmpty()) {
+            IO.println("Shelf code cannot be empty.");
+        } else {
+            this.shelfCode = shelfCode;
+        }
+    }
+
     @Override
     public void printAllInfo() {
         IO.println(
                 "Book: " + title +
                         " by " + author +
                         " (" + pages + " pages) [" +
-                        getShelfCode() + "]"
+                        shelfCode + "]"
         );
     }
+
     @Override
     public String getLabel() {
         return title;
-    }
-    @Override
-    public String getShelfCode() {
-        return "B-01";
     }
 }
